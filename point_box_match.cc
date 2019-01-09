7dee52fe1acaf9d8546d794061db3e31b4961651
Wilson Meng <w5meng@edu.uwaterloo.ca>
2019-01-08 21:44:36 -0500

    define structures for parameters used

---
+++ point_box_match.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <exception>
+#include <cstdlib>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+using namespace std;
+
+// Read in a text file detailing...
+// - dimensions of a rectangular bounding box, with orientation
+
+// - coordinates of point
+
+
+int main(int argc, char const *argv[]) {
+
+}
